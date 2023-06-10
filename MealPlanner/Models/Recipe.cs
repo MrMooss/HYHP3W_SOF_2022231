d62cc59c8648d9e01cd4801fb265936b05c78c5b
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2023-06-10 14:36:02 +0200

    Created Recipe model, changed Recipe property in Meal from string to Recipe.

---
+++ MealPlanner/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealPlanner.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Recipe()
+        {
+            Id= Guid.NewGuid().ToString();
+        }
+    }
+}
