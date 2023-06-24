7c9008f314497a3c3c2849a8031ebf170ecefb99
mrmooss <szattila1998@gmail.com>
2023-06-24 12:01:25 +0200

    ViewModel class

---
+++ Kliens/ViewModel/ViewMeal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kliens.ViewModel
+{
+    class ViewMeal
+    {
+        private string name;
+        private string description;
+        private string imageUrl;
+        private DateTime consumptionDate;
+        private ViewRecipe recipe;
+        private MealType mealType;
+
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public string ImageUrl { get => imageUrl; set => imageUrl = value; }
+        public DateTime ConsumptionDate { get => consumptionDate; set => consumptionDate = value; }
+        public MealType MealType { get => mealType; set => mealType = value; }
+        public virtual ViewRecipe Recipe { get => recipe; set => recipe = value; }
+    }
+}
--- MealPlanner/Controllers/MealApiController.cs
+++ MealPlanner/Controllers/MealApiController.cs
@@ -8,9 +8,41 @@ namespace MealPlanner.Controllers
     public class MealApiController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Meal> Index()
+        public IEnumerable<Meal> Get()
         {
-            return null;
+            return meals;
         }
+
+        static List<Meal> meals = new List<Meal>
+        {
+            new Meal(){Name = "name", ConsumptionDate = DateTime.Now, Description="asd", Recipe = new Recipe() { Description = "asdasd", Name="name recept" } },
+        };
+
+        [HttpPost]
+        public void AddMeal([FromBody] Meal m)
+        {
+            m.Id = Guid.NewGuid().ToString();
+            meals.Add(m);
+        }
+
+        [HttpPut]
+        public void EditMeal([FromBody] Meal m)
+        {
+            var old = meals.FirstOrDefault(t => t.Id == m.Id);
+            old.Name = m.Name;
+            old.Description = m.Description;
+            old.Recipe = m.Recipe;
+            old.ConsumptionDate = m.ConsumptionDate;
+            old.ImageUrl = m.ImageUrl;
+            old.MealType = m.MealType;
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteMeal(string id)
+        {
+            var mealToDelete = meals.FirstOrDefault(t => t.Id == id);
+            meals.Remove(mealToDelete);
+        }
+
     }
 }
