324d1dd3c5bb221d7ffa3ab6397020b320387008
mrmooss <szattila1998@gmail.com>
2023-06-24 10:58:19 +0200

    API Controller

---
+++ MealPlanner/Controllers/MealApiController.cs
@@ -0,0 +1,16 @@
+﻿using MealPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MealApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Meal> Index()
+        {
+            return null;
+        }
+    }
+}
