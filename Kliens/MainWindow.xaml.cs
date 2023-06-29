759856473a7bf6f943cb2a8fd91a2c49a42e5160
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2023-06-29 17:24:31 +0200

    fixed base address for client

--- Kliens/MainWindow.xaml.cs
+++ Kliens/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace Kliens
             DataContext = this;
 
             client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:7289");
+            client.BaseAddress = new Uri("https://localhost:7289");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
               new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
