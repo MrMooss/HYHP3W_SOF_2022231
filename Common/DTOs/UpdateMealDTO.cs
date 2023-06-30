7aa38cd436c0d429641cfd0d4b261ed4e8779e29
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2023-06-30 12:11:04 +0200

    Created Add, Update, Delete actions in HomeController.

---
+++ Common/DTOs/UpdateMealDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DTOs
+{
+    public class UpdateMealDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime ConsumptionDate { get; set; }
+        public MealType MealType { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeDescription { get; set; }
+    }
+}
