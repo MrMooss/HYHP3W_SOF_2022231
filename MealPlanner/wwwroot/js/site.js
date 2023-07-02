e2e1071215416339f308c79112295a53026e3c19
mrmooss <szattila1998@gmail.com>
2023-07-02 16:44:09 +0200

    basic css

--- MealPlanner/wwwroot/js/site.js
+++ MealPlanner/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('.view-recipe').on('click', function (e) {
+        e.preventDefault();
+        var recipeDetails = $(this).siblings('.recipe-details');
+        var hideRecipeButton = $(this).siblings('.hide-recipe');
+
+        recipeDetails.toggle();
+        hideRecipeButton.toggle();
+    });
+
+    $('.hide-recipe').on('click', function (e) {
+        e.preventDefault();
+        var recipeDetails = $(this).siblings('.recipe-details');
+        var viewRecipeButton = $(this).siblings('.view-recipe');
+
+        recipeDetails.hide();
+        $(this).hide();
+        viewRecipeButton.show();
+    });
+});
